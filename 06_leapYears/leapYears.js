24e6f938c3d1b9aa63c0a146638816c9c4ceeb68
Chase <cnickellsumn@gmail.com>
2022-09-22 15:00:32 -0700

    GG EZ

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return (year % 4 === 0 && year % 100 !== 0 || year % 100 === 0 && year % 400 === 0)
 };
+
+// tried dicking around with options to return
+// false if the input wasn't an integer, didn't work
+// it appears if its a number that's a string, it 
+// still treats it like an integer
+
+// LOOKS LIKE IT WORKS IF THE STRING ISNT A NUMBER LFG
 
 // Do not edit below this line
 module.exports = leapYears;
