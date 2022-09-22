a46400715531ceffdc7a862dd56900ec7457a2d0
Chase <cnickellsumn@gmail.com>
2022-09-22 10:33:52 -0700

    LFG IM FIGURING IT OUT

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+// LFG EVERYTHING WORKS SO FAR
+// now just need to add condition that checks if 
+// either are neg numbers or aren't numbers at all
 
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a == b) {
+        sum = a;
+    } else if (a > b) {
+        let bigger = a;
+        let smaller = b;
+        for (let i=smaller; i<=bigger; i++) {
+            sum += i;
+        }
+    } else {
+        let bigger = b;
+        let smaller = a;
+        for (let i=smaller; i<=bigger; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
