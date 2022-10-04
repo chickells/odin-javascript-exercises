8c4bea7ea4d978d61389a0cb906d466b98b7d01d
Chase <cnickellsumn@gmail.com>
2022-10-04 13:25:32 -0700

    brain is pretty much but did it. def stole from 'solutions' branch tbh

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1)
 };
 
-const power = function() {
-	
+const power = function(x, n) {
+	return x ** n
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x === 0) return 1;
+  let product = 1;
+  for (let i = x; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
+// for this if function, you don't need the brackets to separate
+// if/else.  if true, return asdfasdf
+// else the other lines of code till return appears
 
 // Do not edit below this line
 module.exports = {
