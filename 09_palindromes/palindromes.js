398b4d1b8b451cc3d272504b961ac5c0cc0e5186
Chase <cnickellsumn@gmail.com>
2022-10-04 13:31:21 -0700

    got my outline!! this stuff makes more sense to me

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const stringToArray = '' // create function here (remove spaces - if character == " ", skip to next index in array)
+    const reversed = stringToArray //.reverseFunction() start at back and subtract from array[array.length] until its array[0]
+    return string === reversed // should return true or false..... *SHOULD* lol
 };
+
+// take string, split it per character
+// make a new array that's reversed
+// check if new array equals original array
+// BOOM EASY
 
 // Do not edit below this line
 module.exports = palindromes;
