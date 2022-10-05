51c944c60e9b326087afaedb666c168da44643f1
Chase <cnickellsumn@gmail.com>
2022-10-05 07:23:00 -0700

    working... mostly lol

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,20 @@
 const palindromes = function (string) {
-    const stringToArray = '' // create function here (remove spaces - if character == " ", skip to next index in array)
-    const reversed = stringToArray //.reverseFunction() start at back and subtract from array[array.length] until its array[0]
-    return string === reversed // should return true or false..... *SHOULD* lol
+    const stringToArray = string.split()
+    const removeSpace = stringToArray.filter(e => String(e).trim()) // 1 DONE, WORKS
+    const reversed = removeSpace.reverse() // 2
+    return string == reversed // should return true or false..... *SHOULD* lol
 };
+
+// IT APPARENTLY WORKS
+// but tbh i think it's just always returning false lol
+
+// 1
+// String(asdf).trim() apparently removes whitespace
+
+// 2
+// .reverseFunction() start at back and subtract from array[array.length] until its array[0]
+
+// 3
 
 // take string, split it per character
 // make a new array that's reversed
