41b6c74c950556ebd29f26be26284dde7e630666
Chase <cnickellsumn@gmail.com>
2022-10-05 18:01:10 -0700

    yeaaahhhh this is hard

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let x = 0
+    for (let i = 0; i < n ; i++) {
+        x = (i - 1)
+        x = (i - 2)
+        console.log(x);
+    }
+    return x
 };
+
+// FUCK THIS SHIT IM GOING HOME Lol
+
+// damn i think we have to make a recursive step that
+// takes in 'n' steps, and we have to loop it that many times
 
 // Do not edit below this line
 module.exports = fibonacci;
