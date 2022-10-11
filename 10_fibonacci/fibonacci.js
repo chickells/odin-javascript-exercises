53bafcc909905b6c44f3f186b74bb41fef1e347e
Chase <cnickellsumn@gmail.com>
2022-10-11 09:49:42 -0700

    IT WORKS BETCH

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,39 +1,27 @@
-// const fibonacci = function(n) {
-//     let x = 0
-//     for (let i = 0; i < n ; i++) {
-//         x = (i - 1)
-//         x = (i - 2)
-//         console.log(x);
-//     }
-//     return x
-// };
-
-function fibonacci (n) {
+function fibonacci (x) {
+    let n = Number(x)
     let fib = []
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i <= n; i++) {
         if (n < 0 || Number.isInteger(n) == false) {
-            fib = 'error, n must be a positive number'
+            fib = 'OOPS'
         } else if (i < 2 ){
             fib.push(i)
-            console.log(i);
-            console.log(fib);
+            // console.log(i);
+            // console.log(fib);
         } else {
             fib.push(fib[i-1]+ fib[i-2])
-            console.log(i);
-            console.log(fib);
+            // console.log(i);
+            // console.log(fib); these check each addition
         }}
-    // if (n = 1) {
-    //     x = 1
-    // } else {
-    //     x = fib[n-1] + fib[n-2]
-    // } 
-    console.log(fib);
-    return fib[n]
+    // console.log(fib); checking array build
+    if (n < 0 || Number.isInteger(n) == false) {
+        return 'OOPS'
+    } else return fib[n]
 }
 
 // holy fuck it works
 
-console.log(fibonacci(14));
+// console.log(fibonacci(14)); checks rando num as example
 
 // create Fib as an array, looping over 'n' times
 
