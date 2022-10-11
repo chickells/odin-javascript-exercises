41e6ce82a2d5e668c712fc198cb524ad604da9ad
Chase <cnickellsumn@gmail.com>
2022-10-11 10:27:53 -0700

    need to brush up on object methods first because i think this is relatively simple but i haven't learned a specific method yet

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,43 @@
-const getTheTitles = function() {
-
+function getTheTitles (object) {
+    // let titles = Object.values(object.title)
+    // return titles
 };
+
+// so basically we need to sift through the object keys (title, author, etc)
+// and return the value of JUST 'title' keys
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+console.log(Object.entries(books));
+
+// this isn't really making sense tbh but i think
+// it's something relatively simple, just a method i haven't learned yet.
+// until next time!
+
+// also, with the 25k... (aka 15-17k remainder after CC debt)
+// would it be beneficial to use it to pay for bills and instead
+// work like 1-2 days a week and do TONS of hw
+// AND/OR do a proper bootcamp??
+
+// because in the long run i think that would net me more future gainz...
+// fack that's def much higher risk tho
+// so either play it safe and put it to student loans,
+// pay for bootcamp/bills while i teach myself
+// upgrade car (probably least smart option tbh)
+// invest
+// combo of everything
+
+// WE GOT TIME TO SORT IT OUT
+// alright lets do next 3 sessions, im hungry af
 
 // Do not edit below this line
 module.exports = getTheTitles;
