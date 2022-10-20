3e9d4cb4d57fa5fed84d3504053c3bc220585d61
Chase <cnickellsumn@gmail.com>
2022-10-20 07:15:22 -0700

    got outline sorted out, now to write code

--- calcProject/app.js
+++ calcProject/app.js
@@ -31,8 +31,33 @@ function operate (operator, num1, num2) {
     return operator(num1, num2)
 }
 
+let operator = ""
 
-console.log(multiply(5,8))
+// operator is a placeholder that's set once you click button
+// function isn't actually called until you press equal
+// variable 'display' is an array that we 'push' digits to.
+// at first it assigns num1
+
+
+// once operator is clicked, next digit click sets display to
+// 'num2' array and starts 'pushing' to that
+// also sets floating 'operator' varible to one of our functions
+
+// once equals is hit, it checks what the current 'operator' is 
+// set to and calls that function, supplying num1 and num2 from our
+// stored numbers in the display.  also updates display to answer.
+
+// from that point there are two options:
+// 1 - click any digit next and it resets display to num1 and resets num1
+// 2 - click equals or another operator again and the prior "result" 
+//     turns into num1 right? and num2 stays constant unless 
+//     another operator was selected
+
+// if the 'clear' button is ever clicked, it resets display to 'num1 = [0]'
+
+// if there's a way to append a digit onto a number (or even string of numbers)
+// that'd be cleaner, otherwise i'm constantly converting this array
+// of num1 and num2 to strings/numbers 
 
 
 
